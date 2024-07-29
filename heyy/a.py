f009b18401639f886991a4a78649e801b58412a5
manhitha17 <manhithapoojary@gmail.com>
2024-07-29 06:37:45 +0530

    d

--- heyy/a.py
+++ heyy/a.py
@@ -18,3 +18,14 @@ a=int(input('Enter'))
 b=int(input('Enter'))
 sum=a+b
 print(sum)
+
+num=11
+if(num>1):
+    for i in range(2,(num//2)+1):
+        if(num%2)==0;
+            print('No')
+            break;
+        else:
+            print('Yes')
+else:
+    print('No')
