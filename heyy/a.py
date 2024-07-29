d54b470c40f96b229a5509a468e7c56696f1b2a8
manhitha17 <manhithapoojary@gmail.com>
2024-07-29 06:26:11 +0530

    p

--- heyy/a.py
+++ heyy/a.py
@@ -4,3 +4,13 @@ def factorial(n):
     else:
         return n*factorial(n-1)
     
+def fibonacci(n):
+    if(n<=0):
+        return "positive no"
+    elif(n==1):
+        return 0
+    elif(n==2):
+        return 1
+    else
+        return fibonacci(n-1)+fibonacci(n-2)
+    
